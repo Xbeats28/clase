3c4dd56a770cd85183280a78db1d3cb64a2cd7fc
Xbeats28 <144905891+Xbeats28@users.noreply.github.com>
2023-10-04 12:53:14 +0200

    Create tumadre.cs

---
+++ tumadre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double espacio, tiempo, velocidad;
+            Console.WriteLine("cual es el espacio que recorre el objeto");
+            espacio = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("en cuanto tiempo lo recorre");
+            tiempo = Convert.ToDouble(Console.ReadLine());
+            velocidad = espacio / tiempo;
+            Console.WriteLine("la velocidad del objeto es" + velocidad + "metros/segundo");
+            Console.ReadLine(); 
+            
+
+        }
+    }
+}
