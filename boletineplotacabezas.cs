9271afa7ad0808d051b20c4150190d80d6486da2
Xbeats28 <144905891+Xbeats28@users.noreply.github.com>
2023-11-13 13:09:37 +0100

    aycavron

--- boletineplotacabezas.cs
+++ boletineplotacabezas.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
@@ -39,7 +41,7 @@ namespace boletin_ni
                     if (nie % 5 == 0 || nie % 7 == 0)
                     {
                         Console.WriteLine(nie);
-                        //EJER==6==CODIGO COPIADO LO SIENTO RAMON
+                        
                     }
 
 
@@ -65,16 +67,77 @@ namespace boletin_ni
                 {
                     Console.WriteLine("Escriba un numero");
                     int a = int.Parse(Console.ReadLine());
-                    for (a=int.Parse(Console.ReadLine());  a <= a+5; a++)
+                    for (; a <= a + 5; a++)
                     {
-                        for (int x = 0; x <= 3; x++)
-                        {
-                            Console.WriteLine(Math.Pow(a, x));
-                        }
+
+                        Console.WriteLine(Math.Pow(a, 1));
+                        Console.WriteLine(Math.Pow(a, 2));
+                        Console.WriteLine(Math.Pow(a, 3));
                     }
 
                 }
-            
+                else if (ejer == 9)
+                {
+                    Console.WriteLine("Raiz de 16 y sus restantes -2.5");
+
+                    for (double n = 16; n >= 1;)
+                    {
+                        Console.WriteLine(Math.Sqrt(n));
+                        n = n - 2.5;
+                    }
+                }
+                else if (ejer == 10)
+                {
+                    Console.WriteLine("Introduzca un valor positivo");
+                    double o = Convert.ToDouble(Console.ReadLine());
+                    if (o > 0)
+                    {
+                        for (int i = 0; i <= 10; i++)
+                        {
+
+                            Console.WriteLine(o + i);
+                        }
+                    }
+                    else { Console.WriteLine("Uno positivo dije"); }
+
+
+
+                }
+                else if (ejer == 11)
+                {
+                    double n, i, l, e;
+
+                    Console.WriteLine("Esciba un numero positivo");
+                    i = double.Parse(Console.ReadLine());
+                    for (n = 1; n <= i; n++)
+                    {
+                        l = 1 / Math.Pow(n, 3);
+                        Console.WriteLine(l);
+                    }
+                }
+                else if (ejer == 12)
+                    Console.WriteLine("Introduzca un numero positivo");
+                double li = Convert.ToDouble(Console.ReadLine());
+                // salir prematuramenet de un bucle --> sentencia break
+                // encontrar el primer multiplo de 3,7 y 8 a la  vez en un rango
+                int limiinf = 1200;
+                int suma2 = 0;
+                int limitsup = 3500;
+                for ( int i = limiinf; i <= limitsup; i++)
+                {
+                    if (i % 3 == 0 && i % 7 == 0 && i % 8 == 0)
+                    {
+                        continue;
+                    }
+                    suma2 = suma2 + i;
+                    
+
+                }
+
+
+                
+                
+                
                 
                 
                 
---
+++ boletintema5part2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parte_2_boletion_9_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char si;
+            do
+            {
+                Console.WriteLine("A que ejercicio quieres ir?");
+                double ejer;
+                ejer = Convert.ToDouble(Console.ReadLine());
+                if (ejer == 13)
+                {
+                    Console.WriteLine("Introduzca un numero por teclado");
+                    double num = Convert.ToDouble(Console.ReadLine());
+                     
+                }
+
+
+
+                Console.WriteLine("Desea continuar? pulse s para seguir o n para parar");
+                si = Convert.ToChar(Convert.ToInt32(Console.ReadLine()));
+            } while (si == 's');
+        }
+    }
+}
